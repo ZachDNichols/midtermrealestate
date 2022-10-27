05c579e7c5554341a63f80bf41fe4d6101e49b92
Zach Nichols <zachdnichols@gmail.com>
2022-10-27 16:13:02 -0500

    Made home page

---
+++ components/header.js
@@ -0,0 +1,107 @@
+const header = document.createElement('template');
+
+header.innerHTML = `
+    <head>
+        <style>
+            .desktop {
+                width: 90%;
+                margin: 0 auto;
+            }
+            
+            header {
+                margin: 0;
+                background-color: blueviolet;
+                color: white;
+                position: sticky;
+
+            }
+            
+            header::after {
+                content: '';
+                display: table;
+                clear: both;
+            }
+            
+            .logo {
+                float: left;
+                padding: 10px 0;
+            }
+            
+            .logo a {
+                color: white;
+                text-decoration: none;
+            }
+            
+            nav.desktopNav ul{
+                margin: 0;
+                padding: 0;
+                list-style: none;
+                float: right;
+            }
+            
+            nav.desktopNav li {
+                display: inline-block;
+                margin-left: 70px;
+                margin-top: 40px;
+                position: relative;
+            }
+            
+            nav.desktopNav a {
+                color: white;
+                text-decoration: none;
+                font-size: larger;
+            }
+            
+            nav.desktopNav a::before {
+                content: '';
+                display: block;
+                height: 3px;
+                background-color: wheat;
+            
+                position: absolute;
+                top: 20px;
+                width: 0%;
+            
+                transition: all ease-in-out 250ms;
+            }
+            
+            nav.desktopNav a:hover {
+                color: wheat
+            }
+            
+            nav.desktopNav a:hover::before {
+                width: 100%;
+            }
+        </style>
+    </head>
+        <header >
+            <div class = "desktop">
+                <h1 class = "logo"><a href = "index.html">Little Country Real Estate</a></h1>
+                <nav class = "desktopNav">
+                    <ul>
+                        <li><a href="">Buy</a></li>
+                        <li><a href="">Rent</a></li>
+                        <li><a href="">Sell</a></li>
+                        <li><a href="">Agents</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        `;
+
+class Header extends HTMLElement 
+{
+    constructor() 
+    {
+        super();
+    }
+
+    connectedCallback() 
+    {
+        const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+        shadowRoot.appendChild(header.content);
+    }
+}
+
+customElements.define('header-component', Header);
